9fb4e502bdc11dd9877a0531fcde7d9dfabd18aa
Martina Boscolo Bacheto <martina.boscolobacheto@studenti.unipd.it>
2025-01-12 19:36:52 +0100

    added safe pose, pregrast, differet shapes of collision object, orientation of the gripper

--- src/node_c.cpp
+++ src/node_c.cpp
@@ -11,9 +11,19 @@
 #include <control_msgs/FollowJointTrajectoryAction.h>
 #include <trajectory_msgs/JointTrajectoryPoint.h>
 #include <actionlib/client/simple_action_client.h>
+#include <iostream>
+#include <string>
 
 const float tiago_start_x = -6.580157;
 const float tiago_start_y = 1.369999;
+
+// define name of models of links of Gazebo objects
+const std::string model_types[9] = {"Hexagon", "Hexagon_2", "Hexagon_3", "cube", "cube_5",
+	"cube_6", "Triangle", "Triangle_8", "Triangle_9"};
+
+const std::string link_types[9] = {"Hexagon_link", "Hexagon_2_link", "Hexagon_3_link", "cube_link", "cube_5_link",
+	"cube_6_link", "Triangle_link", "Triangle_8_link", "Triangle_9_link"};
+
 
 // TODO
 //  class Manipulation
@@ -21,9 +31,106 @@ const float tiago_start_y = 1.369999;
 //  };
 
 actionlib::SimpleActionServer<ir2425_group_08::PickAndPlaceAction> *as_ptr;
-
 ros::ServiceClient attachService_;
 ros::ServiceClient detachService_;
+
+
+void armInSafePosition()
+{
+    moveit::planning_interface::MoveGroupInterface move_group("arm_torso");
+
+    std::map<std::string, double> target_joint_values;
+    target_joint_values["torso_lift_joint"] = 0.272; // meters
+    target_joint_values["arm_1_joint"] = 27.0 * M_PI / 180.0; // Convert degrees to radians
+    target_joint_values["arm_2_joint"] = 33.0 * M_PI / 180.0;
+    target_joint_values["arm_3_joint"] = -163.0 * M_PI / 180.0;
+    target_joint_values["arm_4_joint"] = 83.0 * M_PI / 180.0;
+    target_joint_values["arm_5_joint"] = -83.0 * M_PI / 180.0;
+    target_joint_values["arm_6_joint"] = 14.0 * M_PI / 180.0;
+    target_joint_values["arm_7_joint"] = 33.0 * M_PI / 180.0;
+
+    move_group.setJointValueTarget(target_joint_values);
+
+    moveit::planning_interface::MoveGroupInterface::Plan plan;
+    if (move_group.plan(plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS)
+    {
+        ROS_INFO("Arm in safe position successful. Executing...");
+        move_group.execute(plan);
+    }
+    else
+    {
+        ROS_ERROR("Arm in safe position failed.");
+    }
+}
+
+void armInPregraspPosition()
+{
+    moveit::planning_interface::MoveGroupInterface move_group("arm_torso");
+
+    std::map<std::string, double> target_joint_values;
+    target_joint_values["torso_lift_joint"] = 0.272; // meters
+    target_joint_values["arm_1_joint"] = 27.0 * M_PI / 180.0; // Convert degrees to radians
+    target_joint_values["arm_2_joint"] = 33.0 * M_PI / 180.0;
+    target_joint_values["arm_3_joint"] = -163.0 * M_PI / 180.0;
+    target_joint_values["arm_4_joint"] = 83.0 * M_PI / 180.0;
+    target_joint_values["arm_5_joint"] = -83.0 * M_PI / 180.0;
+    target_joint_values["arm_6_joint"] = 14.0 * M_PI / 180.0;
+    target_joint_values["arm_7_joint"] = 33.0 * M_PI / 180.0;
+
+    move_group.setJointValueTarget(target_joint_values);
+
+    moveit::planning_interface::MoveGroupInterface::Plan plan;
+    if (move_group.plan(plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS)
+    {
+        ROS_INFO("Arm in pre grasp position successful. Executing...");
+        move_group.execute(plan);
+    }
+    else
+    {
+        ROS_ERROR("Arm in pre grasp position failed.");
+    }
+}
+
+//aggiungere il for per creare tutti i collision object ( il goal deve cambiare)
+void addCollisionObject(const ir2425_group_08::PickAndPlaceGoalConstPtr &goal )
+{
+    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+    // for (const auto& obj : goal->detectedObj)
+    // {   // Cycle through all detected objects
+
+    moveit_msgs::CollisionObject obj;
+    obj.header.frame_id = "base_link";
+    obj.id = std::to_string(goal->id);
+
+    geometry_msgs::Pose obj_pose;
+    obj_pose.position.x = goal->goal_pose.position.x;
+    obj_pose.position.y = goal->goal_pose.position.y;
+    obj_pose.position.z = goal->goal_pose.position.z;
+    obj_pose.orientation = goal->goal_pose.orientation;
+
+    shape_msgs::SolidPrimitive obj_shape;
+
+    if (goal->id < 1 || goal->id > 9){
+        ROS_ERROR("ERROR  | Object with id %d not supported", goal->id);
+        return;
+    } else if (goal->id <= 3 && goal->id >= 1) {
+        obj_shape.type = obj_shape.CYLINDER;
+        obj_shape.dimensions = {0.07, 0.05};
+    } else if (goal->id <= 6 && goal->id >= 4) {
+        obj_shape.type = obj_shape.BOX;
+        obj_shape.dimensions = {0.05, 0.05, 0.05};
+    } else if (goal->id <= 9 && goal->id >= 7) {
+        obj_shape.type = obj_shape.BOX;
+        obj_shape.dimensions = {0.07, 0.05, 0.05};
+    }
+
+    obj.primitives.push_back(obj_shape);
+    obj.primitive_poses.push_back(obj_pose);
+    obj.operation = obj.ADD;
+
+    planning_scene_interface.applyCollisionObjects({obj});
+//}
+}
 
 void pickAndPlaceCallback(const ir2425_group_08::PickAndPlaceGoalConstPtr &goal)
 {
@@ -41,10 +148,8 @@ void pickAndPlaceCallback(const ir2425_group_08::PickAndPlaceGoalConstPtr &goal)
     //  ROS_INFO_STREAM("MoveIt planning frame: " << move_group.getPlanningFrame());
     move_group.setPlanningTime(30.0);
 
-    // Add the object to the collision environment
-    moveit_msgs::CollisionObject obj;
-    obj.id = "obj";
-    obj.header.frame_id = "base_link";
+    armInSafePosition();
+    armInPregraspPosition();
 
     geometry_msgs::Pose obj_pose;
     obj_pose.position.x = goal->goal_pose.position.x;
@@ -52,19 +157,26 @@ void pickAndPlaceCallback(const ir2425_group_08::PickAndPlaceGoalConstPtr &goal)
     obj_pose.position.z = goal->goal_pose.position.z;
     obj_pose.orientation = goal->goal_pose.orientation;
 
-    shape_msgs::SolidPrimitive obj_shape;
-    obj_shape.type = obj_shape.BOX;
-    obj_shape.dimensions = {0.05, 0.05, 0.05};
-
-    obj.primitives.push_back(obj_shape);
-    obj.primitive_poses.push_back(obj_pose);
-    obj.operation = obj.ADD;
-
-    planning_scene_interface.applyCollisionObjects({obj});
+    //deve diventare addCollisionObjects
+    addCollisionObject(goal );
 
     // Plan to pre-grasp pose
     geometry_msgs::Pose pre_grasp_pose = obj_pose;
     pre_grasp_pose.position.z += 0.3; // Offset above the object
+
+    tf::Quaternion init_quat(obj_pose.orientation.x, obj_pose.orientation.y, obj_pose.orientation.z, obj_pose.orientation.w);
+    double roll, pitch, yaw;
+    
+	tf::Quaternion final_quat(0,0,0,1);
+    tf::Matrix3x3(init_quat).getRPY(roll, pitch, yaw);
+
+    roll = 0;
+    pitch = M_PI_2;
+    final_quat.setRPY(roll, pitch, yaw);
+    pre_grasp_pose.orientation.w = final_quat.w();
+    pre_grasp_pose.orientation.x = final_quat.x();
+    pre_grasp_pose.orientation.y = final_quat.y();
+    pre_grasp_pose.orientation.z = final_quat.z();
 
     // tf2::Quaternion q_orig, q_rot, q_new;
     //     q_orig.setX(0.0);
@@ -86,6 +198,7 @@ void pickAndPlaceCallback(const ir2425_group_08::PickAndPlaceGoalConstPtr &goal)
     //  orientation.setRPY(0, 1.57, 0); // Roll = 180°, Pitch = 0° blu, Yaw = 0°
 
     move_group.setPoseTarget(pre_grasp_pose);
+
     moveit::planning_interface::MoveGroupInterface::Plan pre_grasp_plan;
     if (move_group.plan(pre_grasp_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS)
     {
@@ -110,8 +223,8 @@ void pickAndPlaceCallback(const ir2425_group_08::PickAndPlaceGoalConstPtr &goal)
     gripper_goal.trajectory.joint_names.push_back("gripper_right_finger_joint");
 
     trajectory_msgs::JointTrajectoryPoint open_point;
-    open_point.positions.push_back(0.04);            // Open position for left finger (meters/radians)
-    open_point.positions.push_back(0.04);            // Open position for right finger (meters/radians)
+    open_point.positions.push_back(0.05);            // Open position for left finger (meters/radians)
+    open_point.positions.push_back(0.05);            // Open position for right finger (meters/radians)
     open_point.time_from_start = ros::Duration(1.0); // 1-second movement duration
     gripper_goal.trajectory.points.push_back(open_point);
 
@@ -129,8 +242,8 @@ void pickAndPlaceCallback(const ir2425_group_08::PickAndPlaceGoalConstPtr &goal)
         return;
     }
 
-    geometry_msgs::Pose grasp_pose = obj_pose;
-    grasp_pose.position.z += 0.15;
+    geometry_msgs::Pose grasp_pose = pre_grasp_pose;
+    grasp_pose.position.z -= 0.08;
     // se si cambia orientazione sopra va cambiata anche qui
 
     move_group.setPoseTarget(grasp_pose);
@@ -146,6 +259,8 @@ void pickAndPlaceCallback(const ir2425_group_08::PickAndPlaceGoalConstPtr &goal)
         return;
     }
     gripper_goal.trajectory.points.clear(); // Clear the previous trajectory
+
+    
     trajectory_msgs::JointTrajectoryPoint close_point;
     close_point.positions.push_back(0.0);             // Closed position for left finger
     close_point.positions.push_back(0.0);             // Closed position for right finger
@@ -163,15 +278,15 @@ void pickAndPlaceCallback(const ir2425_group_08::PickAndPlaceGoalConstPtr &goal)
     else
     {
         ROS_WARN("Failed to close the gripper.");
-        return;
+        //return;
     }
 
     gazebo_ros_link_attacher::Attach attach_srv_msg;
     attach_srv_msg.request.model_name_1 = "tiago";
     attach_srv_msg.request.link_name_1 = "arm_7_link";
 
-    attach_srv_msg.request.model_name_2 = "cube_5";
-    attach_srv_msg.request.link_name_2 = "cube_5_link";
+    attach_srv_msg.request.model_name_2 = model_types[goal->id -1];
+    attach_srv_msg.request.link_name_2 = link_types[goal->id -1];
 
     // Check if object is attached
     if (attachService_.call(attach_srv_msg))
@@ -179,17 +294,41 @@ void pickAndPlaceCallback(const ir2425_group_08::PickAndPlaceGoalConstPtr &goal)
         ROS_INFO("Attached object");
     }
 
+    armInSafePosition();
+    armInPregraspPosition();
+
     // Raise the arm with the object
     move_group.setPoseTarget(pre_grasp_pose);
     move_group.move();
+
+    gripper_goal.trajectory.points.clear(); // Clear the previous trajectory
+
+
+    gripper_goal.trajectory.points.push_back(open_point);
+
+    gripper_goal.trajectory.header.stamp = ros::Time::now();
+    gripper_client.sendGoal(gripper_goal);
+    gripper_client.waitForResult();
+
+    if (gripper_client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    {
+        ROS_INFO("Gripper opened successfully. Object grasped!");
+    }
+    else
+    {
+        ROS_WARN("Failed to opened the gripper.");
+        //return;
+    }
 
     if (detachService_.call(attach_srv_msg))
     {
         ROS_INFO("Detached object");
     }
 
+
+
     // Remove the object from the collision environment
-    planning_scene_interface.removeCollisionObjects({obj.id});
+    planning_scene_interface.removeCollisionObjects({std::to_string(goal->id)});
 
     // Indicate success
     ir2425_group_08::PickAndPlaceResult result;
