7c17b9b9395f5856817924ad40ed7adf5c7e818d
Martina Boscolo Bacheto <martina.boscolobacheto@studenti.unipd.it>
2025-01-13 00:20:43 +0100

    custom pregrasp and grasp depending on th shape of the object ( to be improved)

--- src/node_c.cpp
+++ src/node_c.cpp
@@ -101,7 +101,7 @@ void addCollisionObject(const ir2425_group_08::PickAndPlaceGoalConstPtr &goal )
     moveit_msgs::CollisionObject obj;
     obj.header.frame_id = "base_link";
     obj.id = std::to_string(goal->id);
-
+//TODO: TUTTI GLI OGGETTI DEVONO ESSERE SPOSTATI DI METÀ ALTEZZA ALTRIMENTI SONO CREATI CON L'APRILTAG AL CENTRO
     geometry_msgs::Pose obj_pose;
     obj_pose.position.x = goal->goal_pose.position.x;
     obj_pose.position.y = goal->goal_pose.position.y;
@@ -115,21 +115,180 @@ void addCollisionObject(const ir2425_group_08::PickAndPlaceGoalConstPtr &goal )
         return;
     } else if (goal->id <= 3 && goal->id >= 1) {
         obj_shape.type = obj_shape.CYLINDER;
-        obj_shape.dimensions = {0.07, 0.05};
+        obj_shape.dimensions = {0.1, 0.05};
+        obj_pose.position.z -=0.05;
     } else if (goal->id <= 6 && goal->id >= 4) {
         obj_shape.type = obj_shape.BOX;
         obj_shape.dimensions = {0.05, 0.05, 0.05};
+        obj_pose.position.z -=0.025;
     } else if (goal->id <= 9 && goal->id >= 7) {
         obj_shape.type = obj_shape.BOX;
-        obj_shape.dimensions = {0.07, 0.05, 0.05};
+        obj_shape.dimensions = {0.05, 0.05, 0.05};
+        obj_pose.position.z -=0.015; //approx
     }
-
     obj.primitives.push_back(obj_shape);
     obj.primitive_poses.push_back(obj_pose);
     obj.operation = obj.ADD;
 
     planning_scene_interface.applyCollisionObjects({obj});
 //}
+}
+
+void addGlobalCollisionObject( )
+{
+    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+
+    // Add the pick_table to the collision environment
+    moveit_msgs::CollisionObject pick_table;
+    pick_table.id = "pick_table";
+    pick_table.header.frame_id = "map";
+
+    geometry_msgs::Pose pick_table_pose;
+    pick_table_pose.position.x = 1.332080 - tiago_start_x;
+    pick_table_pose.position.y = -1.646500 - tiago_start_y;
+    pick_table_pose.position.z = 0.4;
+    pick_table_pose.orientation.w = 1.0;
+
+    shape_msgs::SolidPrimitive pick_table_shape;
+    pick_table_shape.type = pick_table_shape.BOX;
+    pick_table_shape.dimensions = {0.9, 0.9, pick_table_pose.position.z * 2};
+
+    pick_table.primitives.push_back(pick_table_shape);
+    pick_table.primitive_poses.push_back(pick_table_pose);
+    pick_table.operation = pick_table.ADD;
+
+    // Add the place_table to the collision environment
+    moveit_msgs::CollisionObject place_table;
+    place_table.id = "place_table";
+    place_table.header.frame_id = "map";
+
+    geometry_msgs::Pose place_table_pose;
+    place_table_pose.position.x = 1.315500 - tiago_start_x;
+    place_table_pose.position.y = -0.553829 - tiago_start_y;
+    place_table_pose.position.z = 0.4;
+    place_table_pose.orientation.w = 1.0;
+
+    shape_msgs::SolidPrimitive place_table_shape;
+    place_table_shape.type = place_table_shape.BOX;
+    place_table_shape.dimensions = {0.9, 0.9, place_table_pose.position.z * 2};
+
+    place_table.primitives.push_back(place_table_shape);
+    place_table.primitive_poses.push_back(place_table_pose);
+    place_table.operation = place_table.ADD;
+
+    planning_scene_interface.applyCollisionObjects({pick_table, place_table});
+}
+
+geometry_msgs::Pose goToPreGrasp(const ir2425_group_08::PickAndPlaceGoalConstPtr &goal)
+{
+    moveit::planning_interface::MoveGroupInterface move_group("arm_torso");
+    move_group.setPoseReferenceFrame("base_link");
+    move_group.setPlanningTime(30.0);
+
+    geometry_msgs::Pose obj_pose;
+    obj_pose.position.x = goal->goal_pose.position.x;
+    obj_pose.position.y = goal->goal_pose.position.y;
+    obj_pose.position.z = goal->goal_pose.position.z;
+    obj_pose.orientation = goal->goal_pose.orientation;
+
+    
+    geometry_msgs::Pose pre_grasp_pose = obj_pose;
+
+    tf::Quaternion init_quat(obj_pose.orientation.x, obj_pose.orientation.y, obj_pose.orientation.z, obj_pose.orientation.w);
+    double roll, pitch, yaw;
+    tf::Quaternion final_quat(0,0,0,1);
+    tf::Matrix3x3(init_quat).getRPY(roll, pitch, yaw);
+
+    if (goal->id < 1 || goal->id > 9)
+    {
+        ROS_ERROR("ERROR  | Object with id %d not supported", goal->id);
+    }
+    else if (goal->id <= 3 && goal->id >= 1)
+    {
+        roll = 0;
+        pitch = M_PI_2;
+        pre_grasp_pose.position.z += 0.3; 
+        ROS_INFO("hexagon");
+    }
+    else if (goal->id <= 6 && goal->id >= 4)
+    {
+        roll = 0;
+        pitch = M_PI_2;
+        pre_grasp_pose.position.z += 0.3;
+        ROS_INFO("cube"); 
+    }
+    else if (goal->id <= 9 && goal->id >= 7)
+    {
+        roll = 0;
+        pitch = M_PI_2;
+        //yaw += M_PI_2;
+        pre_grasp_pose.position.z += 0.3;
+        ROS_INFO("triangle");
+    }
+
+    final_quat.setRPY(roll, pitch, yaw);
+    pre_grasp_pose.orientation.w = final_quat.w();
+    pre_grasp_pose.orientation.x = final_quat.x();
+    pre_grasp_pose.orientation.y = final_quat.y();
+    pre_grasp_pose.orientation.z = final_quat.z();
+
+    move_group.setPoseTarget(pre_grasp_pose);
+
+    moveit::planning_interface::MoveGroupInterface::Plan pre_grasp_plan;
+    if (move_group.plan(pre_grasp_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS)
+    {
+        ROS_INFO("Planning to pre-grasp pose successful. Executing...");
+        move_group.execute(pre_grasp_plan);
+    }
+    else
+    {
+        ROS_ERROR("Planning to pre-grasp pose failed."); 
+    }
+    return pre_grasp_pose;
+}
+
+
+void goToGrasp(const geometry_msgs::Pose pre_grasp_pose, const int goal){
+
+    moveit::planning_interface::MoveGroupInterface move_group("arm_torso");
+    move_group.setPoseReferenceFrame("base_link");
+    move_group.setPlanningTime(30.0);
+    
+    geometry_msgs::Pose grasp_pose = pre_grasp_pose;
+
+
+    if (goal < 1 || goal > 9)
+    {
+        ROS_ERROR("ERROR  | Object with id %d not supported", goal);
+    }
+    else if (goal <= 3 && goal >= 1)
+    {
+        grasp_pose.position.z -= 0.03; 
+        ROS_INFO("hexagon");
+    }
+    else if (goal <= 6 && goal >= 4)
+    {
+        grasp_pose.position.z -= 0.08;
+        ROS_INFO("cube"); 
+    }
+    else if (goal <= 9 && goal >= 7)
+    {
+        grasp_pose.position.z -= 0.7;
+        ROS_INFO("triangle");
+    }
+
+    move_group.setPoseTarget(grasp_pose);
+    moveit::planning_interface::MoveGroupInterface::Plan grasp_plan;
+    if (move_group.plan(grasp_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS)
+    {
+        ROS_INFO("Planning to grasp pose successful. Executing...");
+        move_group.execute(grasp_plan);
+    }
+    else
+    {
+        ROS_ERROR("Planning to grasp pose failed.");
+        return;
+    }
 }
 
 void pickAndPlaceCallback(const ir2425_group_08::PickAndPlaceGoalConstPtr &goal)
@@ -158,59 +317,11 @@ void pickAndPlaceCallback(const ir2425_group_08::PickAndPlaceGoalConstPtr &goal)
     obj_pose.orientation = goal->goal_pose.orientation;
 
     //deve diventare addCollisionObjects
-    addCollisionObject(goal );
+    addCollisionObject(goal);
 
     // Plan to pre-grasp pose
-    geometry_msgs::Pose pre_grasp_pose = obj_pose;
-    pre_grasp_pose.position.z += 0.3; // Offset above the object
-
-    tf::Quaternion init_quat(obj_pose.orientation.x, obj_pose.orientation.y, obj_pose.orientation.z, obj_pose.orientation.w);
-    double roll, pitch, yaw;
-    
-	tf::Quaternion final_quat(0,0,0,1);
-    tf::Matrix3x3(init_quat).getRPY(roll, pitch, yaw);
-
-    roll = 0;
-    pitch = M_PI_2;
-    final_quat.setRPY(roll, pitch, yaw);
-    pre_grasp_pose.orientation.w = final_quat.w();
-    pre_grasp_pose.orientation.x = final_quat.x();
-    pre_grasp_pose.orientation.y = final_quat.y();
-    pre_grasp_pose.orientation.z = final_quat.z();
-
-    // tf2::Quaternion q_orig, q_rot, q_new;
-    //     q_orig.setX(0.0);
-    //     q_orig.setY(0.0);
-    //     q_orig.setZ(0.0);
-    //     q_orig.setW(1.0); // Identity quaternion (no rotation)
-    //     double roll = M_PI / 2;            // No roll adjustment
-    //     double pitch = M_PI / 2;    // 90 degrees
-    //     double yaw = M_PI / 2;             // No yaw adjustment
-    //     q_rot.setRPY(roll, pitch, yaw);
-    //     // Combine the original orientation with the desired rotation
-    //     q_new = q_rot * q_orig; // Order matters: q_rot * q_orig applies the rotation
-    //     q_new.normalize();      // Ensure the quaternion is normalized
-    //     pre_grasp_pose.orientation.x = q_new.x();
-    //     pre_grasp_pose.orientation.y = q_new.y();
-    //     pre_grasp_pose.orientation.z = q_new.z();
-    //     pre_grasp_pose.orientation.w = q_new.w();
-
-    //  orientation.setRPY(0, 1.57, 0); // Roll = 180°, Pitch = 0° blu, Yaw = 0°
-
-    move_group.setPoseTarget(pre_grasp_pose);
-
-    moveit::planning_interface::MoveGroupInterface::Plan pre_grasp_plan;
-    if (move_group.plan(pre_grasp_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS)
-    {
-        ROS_INFO("Planning to pre-grasp pose successful. Executing...");
-        move_group.execute(pre_grasp_plan);
-    }
-    else
-    {
-        ROS_ERROR("Planning to pre-grasp pose failed.");
-        return;
-    }
-
+    geometry_msgs::Pose pre_grasp_pose = goToPreGrasp(goal); 
+  
     actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> gripper_client("/gripper_controller/follow_joint_trajectory", true);
 
     ROS_INFO("Waiting for the gripper action server...");
@@ -242,22 +353,23 @@ void pickAndPlaceCallback(const ir2425_group_08::PickAndPlaceGoalConstPtr &goal)
         return;
     }
 
-    geometry_msgs::Pose grasp_pose = pre_grasp_pose;
-    grasp_pose.position.z -= 0.08;
-    // se si cambia orientazione sopra va cambiata anche qui
+    goToGrasp(pre_grasp_pose, goal->id);
+    // geometry_msgs::Pose grasp_pose = pre_grasp_pose;
+    // grasp_pose.position.z -= 0.08;
+    // // se si cambia orientazione sopra va cambiata anche qui
 
-    move_group.setPoseTarget(grasp_pose);
-    moveit::planning_interface::MoveGroupInterface::Plan grasp_plan;
-    if (move_group.plan(grasp_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS)
-    {
-        ROS_INFO("Planning to grasp pose successful. Executing...");
-        move_group.execute(grasp_plan);
-    }
-    else
-    {
-        ROS_ERROR("Planning to grasp pose failed.");
-        return;
-    }
+    // move_group.setPoseTarget(grasp_pose);
+    // moveit::planning_interface::MoveGroupInterface::Plan grasp_plan;
+    // if (move_group.plan(grasp_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS)
+    // {
+    //     ROS_INFO("Planning to grasp pose successful. Executing...");
+    //     move_group.execute(grasp_plan);
+    // }
+    // else
+    // {
+    //     ROS_ERROR("Planning to grasp pose failed.");
+    //     return;
+    // }
     gripper_goal.trajectory.points.clear(); // Clear the previous trajectory
 
     
@@ -348,50 +460,9 @@ int main(int argc, char **argv)
     // Initialize MoveIt interfaces
     // moveit::planning_interface::MoveGroupInterface arm_group("arm_torso");
     // moveit::planning_interface::MoveGroupInterface gripper_group("gripper");
-    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-
     // arm_group.setPlanningTime(10.0);
     // gripper_group.setPlanningTime(10.0);
-
-    // Add the pick_table to the collision environment
-    moveit_msgs::CollisionObject pick_table;
-    pick_table.id = "pick_table";
-    pick_table.header.frame_id = "map";
-
-    geometry_msgs::Pose pick_table_pose;
-    pick_table_pose.position.x = 1.332080 - tiago_start_x;
-    pick_table_pose.position.y = -1.646500 - tiago_start_y;
-    pick_table_pose.position.z = 0.4;
-    pick_table_pose.orientation.w = 1.0;
-
-    shape_msgs::SolidPrimitive pick_table_shape;
-    pick_table_shape.type = pick_table_shape.BOX;
-    pick_table_shape.dimensions = {0.9, 0.9, pick_table_pose.position.z * 2};
-
-    pick_table.primitives.push_back(pick_table_shape);
-    pick_table.primitive_poses.push_back(pick_table_pose);
-    pick_table.operation = pick_table.ADD;
-
-    // Add the place_table to the collision environment
-    moveit_msgs::CollisionObject place_table;
-    place_table.id = "place_table";
-    place_table.header.frame_id = "map";
-
-    geometry_msgs::Pose place_table_pose;
-    place_table_pose.position.x = 1.315500 - tiago_start_x;
-    place_table_pose.position.y = -0.553829 - tiago_start_y;
-    place_table_pose.position.z = 0.4;
-    place_table_pose.orientation.w = 1.0;
-
-    shape_msgs::SolidPrimitive place_table_shape;
-    place_table_shape.type = place_table_shape.BOX;
-    place_table_shape.dimensions = {0.9, 0.9, place_table_pose.position.z * 2};
-
-    place_table.primitives.push_back(place_table_shape);
-    place_table.primitive_poses.push_back(place_table_pose);
-    place_table.operation = place_table.ADD;
-
-    planning_scene_interface.applyCollisionObjects({pick_table, place_table});
+    addGlobalCollisionObject();
 
     actionlib::SimpleActionServer<ir2425_group_08::PickAndPlaceAction> as(nh, "/pick_and_place", pickAndPlaceCallback, false);
     as_ptr = &as;
